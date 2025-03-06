3017d9bce67fa84550ad442aac037915b6c828ba
sebas1913 <sebasosornicastrillon@gmail.com>
2025-03-06 16:33:14 -0500

    feat: create HomeTemplate component.

---
+++ src/components/templates/homeTemplate/HomeTemplate.tsx
@@ -0,0 +1,11 @@
+import Banner from "@/components/organisms/banner/Banner";
+import Steps from "@/components/organisms/steps/Steps";
+
+export default function HomeTemplate() {
+    return (
+        <>
+            <Banner/>
+            <Steps/>
+        </>
+    )
+}
